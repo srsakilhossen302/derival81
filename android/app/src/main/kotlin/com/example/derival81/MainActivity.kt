048ca018d9b96296efc65f02753fdf8fe5a358e2
srsakilhossen302 <cse.sakilhossem@gmail.com>
2026-03-15 01:18:49 +0600

    Initialize project structure and app icons

---
+++ android/app/src/main/kotlin/com/example/derival81/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.derival81
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Utils/AppIcons/app_icons.dart
@@ -0,0 +1,15 @@
+class AppIcons {
+  static const String basePath = "assets/icons";
+
+  static const String appsIcons = "$basePath/AppsIcons.svg";
+  static const String bankIcons = "$basePath/Bank-icons.svg";
+  static const String clIcons = "$basePath/CL-icons.svg";
+  static const String debitCreditCardIcon = "$basePath/Debit-Credit-card-Icon.svg";
+  static const String dwIcons = "$basePath/Dw-icons.svg";
+  static const String facebookIcons = "$basePath/Facebook-Icons.svg";
+  static const String googleIcons = "$basePath/Google-Icons.svg";
+  static const String locationIcons = "$basePath/Location-icons.svg";
+  static const String plasIcons = "$basePath/Plas-Icons.svg";
+  static const String profileIcons = "$basePath/Profile-icons.svg";
+  static const String rtIcons = "$basePath/R-T Icons.svg";
+}
